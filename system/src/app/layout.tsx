c771d6d27f8c0a4afef5b0c8ac4d54c09df7eb1c
Jason Ribble <git@j6r.org>
2026-09-02 11:54:29 -0700

    Revert "chore: break pipeline"
    This reverts commit 87959d1a59a2097b7b046474ac19bf9279ad5060.

--- system/src/app/layout.tsx
+++ system/src/app/layout.tsx
@@ -7,11 +7,13 @@ export const metadata: Metadata = {
   description: "EventsCompanion application",
 };
 
-export default function RootLayout({ children, }: {
-
-
-  return(
-    <html lang = "en" >
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
       <head>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
